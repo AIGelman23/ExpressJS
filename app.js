6576ba9f123e5865cf04b322e13f50e0aef72de0
AIGelman23 <AIGelman23@gmail.com>
2021-06-30 00:23:06 -0400

    views

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -11,7 +14,8 @@ app.use('/admin', adminRoutes); // router object being exported
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+ res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 }); 
+
 
 app.listen(3000); 
