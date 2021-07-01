be6e43de99a8cbad40d01e3f2566ddcc81582666
AIGelman23 <AIGelman23@gmail.com>
2021-07-01 14:29:13 -0400

    dynamic templates

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const express = require('express');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes); // router object being exported
+app.use('/admin', adminData.routes); // router object being exported
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
