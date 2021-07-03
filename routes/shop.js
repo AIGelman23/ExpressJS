53e77a51a0cc38f919cd9568ab5e797dc7b4483d
AIGelman23 <AIGelman23@gmail.com>
2021-07-03 18:25:05 -0400

    MVC

--- routes/shop.js
+++ routes/shop.js
@@ -2,24 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', { 
-    prods: products, 
-    pageTitle: 'Shop', 
-    path: '/', 
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  }); 
-  // above .render() method allows us to use default templating engine and return that template
-  // console.log('shop.js', adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
