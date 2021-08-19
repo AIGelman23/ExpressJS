b898384395d60088414a2e87693c6e13e7906b9c
AIGelman23 <AIGelman23@gmail.com>
2021-08-18 23:53:32 -0400

    mysql complete

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Yeshua777*', {
+const sequelize = new Sequelize('sys', 'root', '', {
 	dialect: 'mysql',
 	host: 'localhost'
 });
